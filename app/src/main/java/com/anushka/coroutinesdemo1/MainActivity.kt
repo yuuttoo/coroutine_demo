b9b0003a31475080eabed08a9d48fbc80def32c3
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2023-12-02 20:44:37 +0800

    first commit

---
+++ app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.anushka.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var btnDownloadUserData : Button
+    private lateinit var btnCount : Button
+    private lateinit var tvCount : TextView
+    private lateinit var tvUserMessage : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
+        btnCount = findViewById(R.id.btnCount)
+        tvCount = findViewById(R.id.tvCount)
+        tvUserMessage = findViewById(R.id.tvUserMessage)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+           // Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+           
+        }
+    }
+}
